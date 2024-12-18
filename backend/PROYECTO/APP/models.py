fad9ca86814383df46e9c66ed56aed6e0b61edb1
EmanuelVMec <joshua.vinces5503@utc.edu.ec>
2024-12-18 07:54:10 -0500

    pooosi

--- backend/PROYECTO/APP/models.py
+++ backend/PROYECTO/APP/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
 # Create your models here.
+class Product(models.Model):
+    nombre = models.CharField(max_length=255, verbose_name="Nombre")
+    descripcion = models.TextField(verbose_name="Descripción", blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    cantidad = models.PositiveIntegerField(verbose_name="Cantidad")
+    pais_de_origen = models.CharField(max_length=255, verbose_name="País de Origen")
+    atamaños_disponibles = models.TextField(verbose_name="Tamaños Disponibles", blank=True, null=True)
+    categorias = models.CharField(max_length=255, verbose_name="Categoría")
+
+    def __str__(self):
+        return self.nombre
+
+class ProductPhoto(models.Model):
+    producto = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="fotos")
+    foto = models.ImageField(upload_to="product_photos/", verbose_name="Foto del Producto")
+
+    def __str__(self):
+        return f"Foto de {self.producto.nombre}"
+
--- backend/PROYECTO/APP/views.py
+++ backend/PROYECTO/APP/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Product, ProductPhoto
 
-# Create your views here.
+# Serializers
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ProductPhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductPhoto
+        fields = '__all__'
+
+# ViewSets
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    @action(detail=True, methods=['get'])
+    def photos(self, request, pk=None):
+        """
+        Retrieve all photos for a specific product.
+        """
+        product = get_object_or_404(Product, pk=pk)
+        photos = product.fotos.all()
+        serializer = ProductPhotoSerializer(photos, many=True)
+        return Response(serializer.data)
+
+class ProductPhotoViewSet(viewsets.ModelViewSet):
+    queryset = ProductPhoto.objects.all()
+    serializer_class = ProductPhotoSerializer
